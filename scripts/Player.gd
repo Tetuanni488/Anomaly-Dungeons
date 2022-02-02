72917086245bbf3411e04ccad778ea70e50934a9
solayman_stp <saharmim@stp.es>
2022-02-02 12:58:03 +0100

    stp

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 const PLAYER_SPEED = 6
+const ROT_SPEED = 7
 
 var player = {
 	velocity = Vector3(0,0,0)
@@ -8,14 +9,16 @@ var player = {
 
 func _ready():
 	pass
-	
+
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		player.velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
 		player.velocity.x = PLAYER_SPEED
+		$MeshInstance.rotate_x(deg2rad(ROT_SPEED))
 	elif Input.is_action_pressed("ui_left"):
 		player.velocity.x = -PLAYER_SPEED
+		$MeshInstance.rotate_x(deg2rad(-ROT_SPEED))
 	else:
 		player.velocity.x = lerp(player.velocity.x,0,0.1)
 	
@@ -23,8 +26,10 @@ func _physics_process(delta):
 		player.velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
 		player.velocity.z = -PLAYER_SPEED
+		$MeshInstance.rotate_z(deg2rad(-ROT_SPEED))
 	elif Input.is_action_pressed("ui_down"):
 		player.velocity.z = PLAYER_SPEED
+		$MeshInstance.rotate_z(deg2rad(ROT_SPEED))
 	else:
 		player.velocity.z = lerp(player.velocity.z,0,0.1)
 	
